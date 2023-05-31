7c233f58655a553af447be1a729cf2e8e386229a
GiannisPeppas <peppasgia@gmail.com>
2023-05-31 12:34:39 +0300

    100%

--- Bonus/Bonus Basketball/1d/Comparator.h
+++ Bonus/Bonus Basketball/1d/Comparator.h
@@ -10,19 +10,16 @@ template<typename T>class Comparator{
         vector<T> vector1;
         vector<T> vector2;
     public:
-        Comparator(const vector<T>& v1,const vector<T>& v2);
-        bool equals() const;
-        bool operator!=(const Comparator<T>& other) const {return !equals(other);}
-        bool operator!=(const T& value) const {return !equals(value);}
-        bool equals(const T& value) const {return vector1 == vector2 && vector1 == value;}
+        Comparator(vector<T>& v1,vector<T>& v2);
+        bool equals();
 };
 
-template<typename T> Comparator<T>::Comparator(const vector<T>& v1,const vector<T>& v2){
+template<typename T> Comparator<T>::Comparator(vector<T>& v1,vector<T>& v2){
     vector1=v1;
     vector2=v2;
 }
 
-template<typename T> bool Comparator<T>::equals() const{
+template<typename T> bool Comparator<T>::equals(){
     if (vector1.size()!=vector2.size())
         return false;
     for (int i=0; i<vector1.size();++i)
